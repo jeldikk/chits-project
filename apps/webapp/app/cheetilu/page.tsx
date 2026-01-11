1294c654b389be953e74cd3e395c9437e3daa0af
jeldikk <jeldi.kamal2013@gmail.com>
2026-01-11 15:38:15 +0530

    feat: align title and action button in the same row on Cheeti landing page

--- apps/webapp/app/cheetilu/page.tsx
+++ apps/webapp/app/cheetilu/page.tsx
@@ -7,10 +7,16 @@ export default async function CheetiluPage() {
   const cheetilu = await fetchCheetilu();
   return (
     <div className="cheetilu-page px-2">
-      <h1 className="text-3xl">Cheeti-lu you have created</h1>
-      <Link href="/cheetilu/create" className="btn btn-primary">
-        Create New
-      </Link>
+      <div className="page-header flex justify-between items-center mb-4">
+        <h1 className="text-3xl">Your Cheeti's</h1>
+        <Link
+          href="/cheetilu/create"
+          className="btn btn-outline btn-secondary btn-sm"
+        >
+          Create New
+        </Link>
+      </div>
+
       <div className="cheeti-list grid grid-cols-3 gap-4">
         {cheetilu.map((cheeti) => (
           <CheetiCard key={cheeti.id} cheeti={cheeti} />
